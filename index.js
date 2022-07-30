f2ab4fbbac0fec8d36463fd3fe8dfa3c9e0009f1
Austin Brock <austinjbrock@gmail.com>
2022-07-29 21:12:34 -0700

    added render function

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import characterData from "./data.js";
-import {getDiceRollArray} from "./utils.js";
+import {getDiceRollArray, getDicePlaceHolderHtml} from "./utils.js";
 import { Character } from "./character.js";
 	
+
+function attack(){
+	render()
+	getDicePlaceHolderHtml()
+}
+
+ document.getElementById('attack-button').addEventListener('click',attack)
+
+
+
+
 const render = function(){
 	document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
  	document.getElementById('monster').innerHTML = orc.getCharacterHtml()
