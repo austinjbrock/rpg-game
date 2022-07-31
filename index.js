2cfd4657799ca08f702a1059b4a484bb71f3ca24
Austin Brock <austinjbrock@gmail.com>
2022-07-30 20:05:34 -0700

    added import for js functions

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { Character } from "./character.js";
 
 function attack(){
 	render()
-	getDicePlaceHolderHtml()
+	
 }
 
  document.getElementById('attack-button').addEventListener('click',attack)
@@ -17,12 +17,14 @@ const render = function(){
 	document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
  	document.getElementById('monster').innerHTML = orc.getCharacterHtml()
 	document.getElementById('knight').innerHTML = knight.getCharacterHtml()
+
 }
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
 const knight = new Character(characterData.knightInArmor)
 
 render()
+
 
 
 
