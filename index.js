d4c8b8738958652b3c3ebabf2b69afcab77a402b
Austin Brock <austinjbrock@gmail.com>
2022-08-03 22:08:19 -0700

    added endgame function

--- index.js
+++ index.js
@@ -4,14 +4,39 @@ import { Character } from "./character.js";
 	
 
 function attack(){
-	render()
 	orc.getDiceHtml()
 	wizard.getDiceHtml()
 	
 	wizard.takeDamage(orc.currentDiceScore)
 	orc.takeDamage(wizard.currentDiceScore)
-
+	render()
+	if(wizard.dead || orc.dead){
+		endGame()
+	}
 }
+
+function endGame(){
+	
+	const endMessage = wizard.health === 0 && orc.health === 0 ? 'No victors - all Players are dead':
+	wizard.health > 0 ? 'Wizard Wins' : 'The Orc is Victorious'
+
+
+	const endEmoji = wizard.health > 0 ? '🔮': '☠️'
+	
+document.body.innerHTML = 
+	`<div class="end-game">
+	<h2>Game Over</h2>
+	<h3>${endMessage}</h3>
+	<p class="end-emoji">${endEmoji}</p>
+	</div>
+	`
+}
+
+
+// const message = playerGuess < correctAnswer ? 'Too low!' : playerGuess > correctAnswer ? 'Too high!' : 'Exactly right!'
+
+// console.log(message)
+
 
  document.getElementById('attack-button').addEventListener('click',attack)
 
