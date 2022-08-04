ef235c9f14d4e3cf1a8ed049f544553e4e672b2b
Austin Brock <austinjbrock@gmail.com>
2022-08-03 22:10:35 -0700

    added get percentage function for health bar

--- character.js
+++ character.js
@@ -5,31 +5,36 @@ function Character(data){
 
       this.diceArray = getDicePlaceHolderHtml(this.diceCount)
 
+      this.maxHealth = this.health
    
       this.getDiceHtml = function(diceCount){
       this.currentDiceScore = getDiceRollArray(this.diceCount)
       
-      this.diceArray = this.currentDiceScore.map(function(num){
-      return `<div class="dice">${num}</div>`
-      }).join(' ')
+      this.diceArray = this.currentDiceScore.map( num => `<div class="dice">${num}</div>`).join(' ')
    
 
       this.takeDamage = function(attackScoreArray){
-        const totalAttackScore = attackScoreArray.reduce(function(total,num) {
-         return total + num
-        })
-       this.health -= totalAttackScore
-   }
- }
-   
+        const totalAttackScore = attackScoreArray.reduce((total,num)=> total + num)
+        this.health -= totalAttackScore;
+          
+       if(this.health <= 0){
+         this.health = 0;
+         this.dead = true
+      }
+      console.log(getPercentage(this.health,this.maxHealth))
+
+}
+
+
+const getPercentage = (remainingHealth,maximumHealth) => (100 * remainingHealth) / maximumHealth
 
 
 
-        
+
+}// end character
+
       this.getCharacterHtml = function(){
          const {elementId, name,avatar,health,diceCount,diceArray} = this;
-         const diceHtml = this.getDiceHtml(diceCount)
-         
          return `
          <div class="character-card">
          <h4 class="name"> ${name} </h4>
